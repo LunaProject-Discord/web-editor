a1a45de2aaf133a4c86bbc8836190ca90e24d369
Aoichaan0513 <aoichaan0513@gmail.com>
2024-09-23 16:05:12 +0900

    [Update] 表への対応 #5

--- src/extensions/index.ts
+++ src/extensions/index.ts
@@ -1,66 +1,8 @@
-import { Extensions } from '@tiptap/core';
-import { BoldExtension } from './bold';
-import { BulletListExtension } from './bullet_list';
-import { CodeExtension } from './code';
-import { DocumentExtension } from './document';
-import { HardBreakExtension } from './hard_break';
-import { HeadingExtension } from './heading';
-import { HistoryExtension } from './history';
-import { ImageExtension } from './image';
-import { ItalicExtension } from './italic';
-import { LinkExtension } from './link';
-import { ListItemExtension } from './list_item';
-import { OrderedListExtension } from './ordered_list';
-import { ParagraphExtension } from './paragraph';
-import { StrikeExtension } from './strike';
-import { SubscriptExtension } from './subscript';
-import { SuperscriptExtension } from './superscript';
-import { TableExtension } from './table';
-import { TableCellExtension } from './table_cell';
-import { TableHeaderExtension } from './table_header';
-import { TableRowExtension } from './table_row';
-import { TaskItemExtension } from './task_item';
-import { TaskListExtension } from './task_list';
-import { TextExtension } from './text';
-import { TextAlignExtension } from './text_align';
-import { UnderlineExtension } from './underline';
-
-export const DefaultExtensions: Extensions = [
-    DocumentExtension,
-    TextExtension,
-
-    HeadingExtension,
-    ParagraphExtension,
-    BulletListExtension,
-    OrderedListExtension,
-    ListItemExtension,
-    TaskListExtension,
-    TaskItemExtension,
-    TableExtension,
-    TableRowExtension,
-    TableCellExtension,
-    TableHeaderExtension,
-    ImageExtension,
-
-    BoldExtension,
-    ItalicExtension,
-    UnderlineExtension,
-    StrikeExtension,
-    CodeExtension,
-    LinkExtension,
-    SubscriptExtension,
-    SuperscriptExtension,
-
-    HistoryExtension,
-    HardBreakExtension,
-    // GapcursorExtension,
-    TextAlignExtension
-];
-
 export * from './bold';
 export * from './bullet_list';
 export * from './code';
 export * from './document';
+export * from './dropcursor';
 export * from './gapcursor';
 export * from './hard_break';
 export * from './heading';
@@ -71,6 +13,7 @@ export * from './link';
 export * from './list_item';
 export * from './ordered_list';
 export * from './paragraph';
+export * from './starter_kit';
 export * from './strike';
 export * from './subscript';
 export * from './superscript';
---
+++ src/extensions/starter_kit/index.ts
@@ -0,0 +1,154 @@
+import { Extension, Extensions } from '@tiptap/core';
+import { BoldOptions } from '@tiptap/extension-bold';
+import { BulletListOptions } from '@tiptap/extension-bullet-list';
+import { CodeOptions } from '@tiptap/extension-code';
+import { HardBreakOptions } from '@tiptap/extension-hard-break';
+import { HeadingOptions } from '@tiptap/extension-heading';
+import { HistoryOptions } from '@tiptap/extension-history';
+import { ImageOptions } from '@tiptap/extension-image';
+import { ItalicOptions } from '@tiptap/extension-italic';
+import { LinkOptions } from '@tiptap/extension-link';
+import { ListItemOptions } from '@tiptap/extension-list-item';
+import { OrderedListOptions } from '@tiptap/extension-ordered-list';
+import { ParagraphOptions } from '@tiptap/extension-paragraph';
+import { StrikeOptions } from '@tiptap/extension-strike';
+import { SubscriptExtensionOptions } from '@tiptap/extension-subscript';
+import { SuperscriptExtensionOptions } from '@tiptap/extension-superscript';
+import { TableOptions } from '@tiptap/extension-table';
+import { TableCellOptions } from '@tiptap/extension-table-cell';
+import { TableHeaderOptions } from '@tiptap/extension-table-header';
+import { TableRowOptions } from '@tiptap/extension-table-row';
+import { TaskItemOptions } from '@tiptap/extension-task-item';
+import { TaskListOptions } from '@tiptap/extension-task-list';
+import { TextAlignOptions } from '@tiptap/extension-text-align';
+import { UnderlineOptions } from '@tiptap/extension-underline';
+import {
+    BoldExtension,
+    BulletListExtension,
+    CodeExtension,
+    DocumentExtension,
+    DropcursorExtension,
+    GapcursorExtension,
+    HardBreakExtension,
+    HeadingExtension,
+    HistoryExtension,
+    ImageExtension,
+    ItalicExtension,
+    LinkExtension,
+    ListItemExtension,
+    OrderedListExtension,
+    ParagraphExtension,
+    StrikeExtension,
+    SubscriptExtension,
+    SuperscriptExtension,
+    TableCellExtension,
+    TableExtension,
+    TableHeaderExtension,
+    TableRowExtension,
+    TaskItemExtension,
+    TaskListExtension,
+    TextAlignExtension,
+    TextExtension,
+    UnderlineExtension
+} from '..';
+
+export interface StarterKitOptions {
+    document: false;
+    text: false;
+
+    heading: Partial<HeadingOptions> | false;
+    paragraph: Partial<ParagraphOptions> | false;
+    bulletList: Partial<BulletListOptions> | false;
+    orderedList: Partial<OrderedListOptions> | false;
+    listItem: Partial<ListItemOptions> | false;
+    taskList: Partial<TaskListOptions> | false;
+    taskItem: Partial<TaskItemOptions> | false;
+    table: Partial<TableOptions> | false;
+    tableRow: Partial<TableRowOptions> | false;
+    tableCell: Partial<TableCellOptions> | false;
+    tableHeader: Partial<TableHeaderOptions> | false;
+    image: Partial<ImageOptions> | false;
+
+    bold: Partial<BoldOptions> | false;
+    italic: Partial<ItalicOptions> | false;
+    underline: Partial<UnderlineOptions> | false;
+    strike: Partial<StrikeOptions> | false;
+    code: Partial<CodeOptions> | false;
+    link: Partial<LinkOptions> | false;
+    subscript: Partial<SubscriptExtensionOptions> | false;
+    superscript: Partial<SuperscriptExtensionOptions> | false;
+
+    history: Partial<HistoryOptions> | false;
+    hardBreak: Partial<HardBreakOptions> | false;
+    gapCursor: false;
+    dropCursor: false;
+    textAlign: Partial<TextAlignOptions> | false;
+}
+
+export const StarterKitExtension = Extension.create<StarterKitOptions>({
+    name: 'starterKit',
+
+    addExtensions() {
+        const extensions: Extensions = [];
+
+        if (this.options.document)
+            extensions.push(DocumentExtension);
+        if (this.options.text)
+            extensions.push(TextExtension);
+
+        if (this.options.heading !== false)
+            extensions.push(HeadingExtension.configure(this.options.heading));
+        if (this.options.paragraph !== false)
+            extensions.push(ParagraphExtension.configure(this.options.paragraph));
+        if (this.options.bulletList !== false)
+            extensions.push(BulletListExtension.configure(this.options.bulletList));
+        if (this.options.orderedList !== false)
+            extensions.push(OrderedListExtension.configure(this.options.orderedList));
+        if (this.options.listItem !== false)
+            extensions.push(ListItemExtension.configure(this.options.listItem));
+        if (this.options.taskList !== false)
+            extensions.push(TaskListExtension.configure(this.options.taskList));
+        if (this.options.taskItem !== false)
+            extensions.push(TaskItemExtension.configure(this.options.taskItem));
+        if (this.options.table !== false)
+            extensions.push(TableExtension.configure(this.options.table));
+        if (this.options.tableRow !== false)
+            extensions.push(TableRowExtension.configure(this.options.tableRow));
+        if (this.options.tableCell !== false)
+            extensions.push(TableCellExtension.configure(this.options.tableCell));
+        if (this.options.tableHeader !== false)
+            extensions.push(TableHeaderExtension.configure(this.options.tableHeader));
+        if (this.options.image !== false)
+            extensions.push(ImageExtension.configure(this.options.image));
+
+        if (this.options.bold !== false)
+            extensions.push(BoldExtension.configure(this.options.bold));
+        if (this.options.italic !== false)
+            extensions.push(ItalicExtension.configure(this.options.italic));
+        if (this.options.underline !== false)
+            extensions.push(UnderlineExtension.configure(this.options.underline));
+        if (this.options.strike !== false)
+            extensions.push(StrikeExtension.configure(this.options.strike));
+        if (this.options.code !== false)
+            extensions.push(CodeExtension.configure(this.options.code));
+        if (this.options.link !== false)
+            extensions.push(LinkExtension.configure(this.options.link));
+        if (this.options.subscript !== false)
+            extensions.push(SubscriptExtension.configure(this.options.subscript));
+        if (this.options.superscript !== false)
+            extensions.push(SuperscriptExtension.configure(this.options.superscript));
+
+        if (this.options.history !== false)
+            extensions.push(HistoryExtension.configure(this.options.history));
+        if (this.options.hardBreak !== false)
+            extensions.push(HardBreakExtension.configure(this.options.hardBreak));
+        if (this.options.gapCursor)
+            extensions.push(GapcursorExtension);
+        if (this.options.dropCursor)
+            extensions.push(DropcursorExtension);
+        if (this.options.textAlign !== false)
+            extensions.push(TextAlignExtension.configure(this.options.textAlign));
+
+        return extensions;
+    }
+});
