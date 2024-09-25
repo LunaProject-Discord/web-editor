5c95890f7c5616c426c65b908b1d43264e34d915
Aoichaan0513 <aoichaan0513@gmail.com>
2024-09-25 22:17:19 +0900

    [Update] デザインの調整

--- src/components/Ribbon/GroupDivider/index.tsx
+++ src/components/Ribbon/GroupDivider/index.tsx
@@ -7,6 +7,6 @@ export const RibbonGroupDivider = ({ orientation, flexItem, sx }: DividerProps) 
     <Divider
         orientation={orientation ?? 'vertical'}
         flexItem={flexItem ?? true}
-        sx={{ my: 1, ...sx }}
+        sx={{ m: 1, ...sx }}
     />
 );
