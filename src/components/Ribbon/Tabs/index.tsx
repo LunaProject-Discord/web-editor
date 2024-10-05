4accf3765181e8343e3f0f13ecf6610189f9dcca
Aoichaan0513 <aoichaan0513@gmail.com>
2024-10-05 21:11:18 +0900

    [Update] RibbonTab のコンテキスト化 #14

--- src/components/Ribbon/Tabs/index.tsx
+++ src/components/Ribbon/Tabs/index.tsx
@@ -1,22 +1,39 @@
 'use client';
 
-import { Tabs } from '@mui/material';
-import React from 'react';
-import { EditorComponentProps, RibbonTab, useRibbonTabContext } from '../../../';
+import { Tabs, TabsActions } from '@mui/material';
+import React, { useCallback } from 'react';
+import { EditorComponentProps, RibbonTab, useCurrentEditor, useRibbonTabContext } from '../../../';
 
 export type RibbonTabsProps = EditorComponentProps;
 
-export const RibbonTabs = ({ editor }: RibbonTabsProps) => {
+export const RibbonTabs = ({ editor: _editor }: RibbonTabsProps) => {
     const { open, name, tabs } = useRibbonTabContext();
+
+    const handleRef = useCallback((actions: TabsActions | null) => {
+        if (!actions || !open)
+            return;
+
+        actions.updateIndicator();
+    }, [open]);
+
+    const editor = useCurrentEditor(_editor);
+    if (!editor)
+        return null;
 
     console.log('[Tabs]', open, name, tabs);
 
     return (
-        <Tabs value={open ? name : undefined} variant="scrollable" scrollButtons="auto" sx={{ border: 'none' }}>
+        <Tabs
+            value={open ? name : undefined}
+            action={handleRef}
+            variant="scrollable"
+            scrollButtons="auto"
+            sx={{ border: 'none' }}
+        >
             {tabs.map((tab) => (
                 <RibbonTab
                     key={tab.name}
-                    editor={editor}
+                    editor={_editor}
                     name={tab.name}
                     label={tab.label}
                     accessKey={tab.accessKey}
