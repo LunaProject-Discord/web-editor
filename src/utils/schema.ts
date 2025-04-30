91eaa05b558bab26ece8c71098ec745c2a277b60
Aoichaan0513 <aoichaan0513@gmail.com>
2025-04-30 12:30:25 +0900

    [Update] 指定したノードが利用可能かどうかを判定するメソッドの追加

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -31,6 +31,17 @@ export const Schema = {
     })
 } as const;
 
+export const isNodeAllowed = (node: PMNode, nodeName: string) => {
+    const nodeType = node.type;
+    const schema = nodeType.schema;
+
+    const type = schema.nodes[nodeName];
+    if (!type)
+        return false;
+
+    return nodeType.contentMatch.matchType(type) !== null;
+};
+
 export const isMarkAllowed = (node: PMNode, markName: string) => {
     const nodeType = node.type;
     const schema = nodeType.schema;
